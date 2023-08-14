6ca55cdadbee7691fcc35e8ce55afee25a0b6e72
unknown <mbsrushti@gmail.com>
2023-08-15 10:41:45 +1200

    next app

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home(){
+    return(
+        <div>
+            <h1>
+                Software Practice Emperical Evidence Database (Speed)
+            </h1>
+        </div>
+    );
+}
